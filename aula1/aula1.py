761b727d537104539b79ba629f78d7ad1c7566c7
mvfsouza <marcossouza_gin@live.com>
2022-10-14 16:00:40 -0300

    initial commit

---
+++ aula1/aula1.py
@@ -0,0 +1,10 @@
+""""
+#  Comentário
+print('linha 1')
+print('linha 2')
+#  Falando o que se trata essa linha
+print('linha 3')
+#  Deixar para depois print('linha 4')  # Isso mostra linha 4 na tela
+print('linha 5')
+print('linha 6')
+"""""
---
+++ aula3/aula3.py
@@ -0,0 +1,10 @@
+"""
+str - string
+"""""
+print("Essa é uma 'string'(str).")
+print('Essa é uma "string"(str).')
+print('Essa é uma \"texto\"(str).')
+print('Essa é uma \'string\' (str).')
+print(r'Esse é \n (str).')
+
+
