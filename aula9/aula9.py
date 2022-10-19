c76b826cf9d6166e4b6487c809c4475a67bdbd3c
mvfsouza <marcossouza_gin@live.com>
2022-10-19 17:54:31 -0300

    aulas novas que fiz do meu trabalho

--- aula9/aula9.py
+++ aula9/aula9.py
@@ -1,1 +1,20 @@
-print ('marcos vinicius')
+''''
+Entrada de dados 
+'''''
+#print(f'O usuário digitou {nome} e o tipo da variável é'
+     # f'{type(nome)}')
+"""""
+nome = input('Qual seu nome? ')
+idade = input('Qual sua idade? ')
+
+ano_nascimento = 2022-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos.'
+      f'{nome} nasceu em {ano_nascimento}')
+"""""
+
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite um número:' ))
+
+print( numero_1 + numero_2)
